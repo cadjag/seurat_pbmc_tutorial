bd2f8569990785b045a607b8fa6980e38a8e44bc
cadjag <wac3aj@virginia.edu>
2023-03-08 16:14:04 -0500

    rmarkdown render

--- R/project_setup.R
+++ R/project_setup.R
@@ -5,6 +5,9 @@ renv::install("markdown")
 renv::install("BiocManager")
 renv::install("tidyverse")
 renv::install("Seurat")
+renv::install("Matrix")
 BiocManager::install("limma")
+
+reticulate::py_install(packages = 'umap-learn')
 
 renv::snapshot()
